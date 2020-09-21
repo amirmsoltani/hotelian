ee12fd9f5048aadd2872fe8948d512ff470dfac4
moodi <moodizone@gmail.com>
2020-09-21 15:10:44 +0330

    nvm

--- src/Lib/IF.ts
+++ src/Lib/IF.ts
@@ -1,43 +1,43 @@
 class IF {
 
+    empty = {};
+    protected response: any;
 
-  protected response: any;
+    public get RES(): any {
+        const res = this.response;
+        this.response = null;
+        return res;
+    }
 
-  public get RES(): any {
-    const res = this.response;
-    this.response = null;
-    return res;
-  }
+    public get OR(): { [key: string]: any } {
+        const res = this.response ? this.response : this.empty;
+        this.response = null;
+        return res;
+    }
 
-  public get OR(): object {
-    const res = this.response ? this.response : {};
-    this.response = null;
-    return res;
-  }
+    O(condition: boolean, object: object): this {
+        if (condition && !this.response)
+            this.response = object;
+        return this;
+    }
 
-  O(condition: boolean, object: object): this {
-    if (condition && !this.response)
-      this.response = object;
-    return this;
-  }
+    IF(condition: boolean, callback: Function): this {
+        if (condition && !this.response)
+            this.response = callback();
+        return this;
+    }
 
-  IF(condition: boolean, callback: Function): this {
-    if (condition && !this.response)
-      this.response = callback();
-    return this;
-  }
+    IFP<T = any>(condition: boolean, item: T, resolve: ((item: T) => any)): this {
+        if (condition && !this.response && item)
+            this.response = resolve(item);
+        return this;
+    }
 
-  IFP<T = any>(condition: boolean, item: T, resolve: ((item: T) => any)): this {
-    if (condition && !this.response && item)
-      this.response = resolve(item);
-    return this;
-  }
-
-  ELSE(callback: Function): this {
-    if (!this.response)
-      this.response = callback();
-    return this;
-  }
+    ELSE(callback: Function): this {
+        if (!this.response)
+            this.response = callback();
+        return this;
+    }
 
 
 }
