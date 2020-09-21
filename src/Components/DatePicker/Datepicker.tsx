8aa1a273cb090699b34e8b1f8afccc9ea5d6feaf
moodi <moodizone@gmail.com>
2020-09-21 15:10:59 +0330

    date picket redesigned

--- src/Components/DatePicker/Datepicker.tsx
+++ src/Components/DatePicker/Datepicker.tsx
@@ -1,133 +1,183 @@
 import React, {Component} from 'react';
-import {FlatList, SafeAreaView, Text, TouchableHighlight, View} from 'react-native';
 import moment, {Moment, MomentInput} from 'moment';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
+import {Button} from "native-base";
+
 import style from './DatepickerStyles';
 import {DateType, Month as MonthType} from '../../Typescript/Types/DatepickerTypes';
 import Month from './Month';
 import IF from '../../Lib/IF';
-
+import {AppText} from "../../Containers";
+import {Style} from '../../Styles';
 
 interface Props {
-  defaultValue?: {
-    checkIn: MomentInput;
-    checkOut: MomentInput;
-  };
-  onSelect: (data: {
-    checkIn: DateType;
-    checkOut: DateType;
-    closeAfter?: boolean;
-  }) => void;
-  format: string;
-};
+    defaultValue?: {
+        checkIn: MomentInput;
+        checkOut: MomentInput;
+    };
+    onSelect: (data: {
+        checkIn: DateType;
+        checkOut: DateType;
+        closeAfter?: boolean;
+    }) => void;
+    format: string;
+}
 
 interface State {
-  checkIn: number;
-  checkOut: number;
-
-
+    checkIn: number;
+    checkOut: number;
 }
 
 class Datepicker extends Component<Props, State> {
-  year: MonthType[];
-  static defaultProps = {
-    defaultValue: {
-      checkIn: moment().format('DD-MM-YYYY'),
-      checkOut: moment().add(1, 'day').format('DD-MM-YYYY'),
-    },
-    format: 'DD-MM-YYYY',
-  };
+    year: MonthType[];
+    nights = 1;
+    static defaultProps = {
+        defaultValue: {
+            checkIn: moment().format('DD-MM-YYYY'),
+            checkOut: moment().add(1, 'day').format('DD-MM-YYYY'),
+        },
+        format: 'DD-MM-YYYY',
+    };
 
-  convertToZero(date: Moment): Moment {
-    date.startOf('hour');
-    date.startOf('minute');
-    date.startOf('second');
-    date.startOf('millisecond');
-    return date;
-  }
-
-  constructor(props: Props) {
-    super(props);
-    const checkIn = this.convertToZero(moment(this.props.defaultValue!.checkIn, this.props.format)).unix();
-    const checkOut = this.convertToZero(moment(this.props.defaultValue!.checkOut, this.props.format)).unix();
-    this.state = {checkIn, checkOut};
-    this.year = this.createOneYear();
-    this.selectDay = this.selectDay.bind(this);
-    this.doneCheck = this.doneCheck.bind(this);
-  }
-
-  createOneMonth(date: Moment): MonthType {
-    const month: MonthType = {name: date.format('MMMM YYYY'), time: date.unix(), days: []};
-    const weekDay = date.day();
-    const days = date.daysInMonth() + weekDay;
-    for (let i = 1; i <= days; i++) {
-      month.days.push({
-        text: weekDay >= i ? '' : i - weekDay,
-        time: weekDay >= i ? 0 : date.unix(),
-      });
-      if (i > weekDay)
-        date.add(1, 'day');
+    constructor(props: Props) {
+        super(props);
+        const checkIn = this.convertToZero(moment(this.props.defaultValue!.checkIn, this.props.format)).unix();
+        const checkOut = this.convertToZero(moment(this.props.defaultValue!.checkOut, this.props.format)).unix();
+        this.state = {checkIn, checkOut};
+        this.year = this.createOneYear();
+        this.selectDay = this.selectDay.bind(this);
+        this.doneCheck = this.doneCheck.bind(this);
     }
-    return month;
-  }
 
-  createOneYear(): MonthType[] {
-    const date = moment();
-    date.startOf('month');
-    this.convertToZero(date);
-    const oneYear: MonthType[] = [];
-    for (let month = 0; month < 12; month++)
-      oneYear.push(this.createOneMonth(date));
-    return oneYear;
-  }
+    convertToZero(date: Moment): Moment {
+        date.startOf('hour');
+        date.startOf('minute');
+        date.startOf('second');
+        date.startOf('millisecond');
+        return date;
+    }
 
-  selectDay(unix: number) {
-    const {checkIn, checkOut} = this.state;
-    if ((checkIn + 1 && checkOut + 1) || checkIn > unix)
-      this.setState({checkIn: unix, checkOut: -1});
-    else if (checkIn + 1 && unix > checkIn)
-      this.setState({checkOut: unix});
-  }
-
-  doneCheck() {
-    const checkIn = moment.unix(this.state.checkIn);
-    const checkOut = moment.unix(this.state.checkOut);
-    this.props.onSelect({
-      checkIn: {formatted: checkIn.format('YYYY MMMM DD'), value: checkIn.format(this.props.format)},
-      checkOut: {formatted: checkOut.format('YYYY MMMM DD'), value: checkOut.format(this.props.format)},
-    });
-  }
-
-  render() {
-    const {checkOut, checkIn} = this.state;
-    return (
-      <View style={style.container}>
-        <View>
-          <Text>{moment(checkIn * 1000).format('YYYY MMMM DD')}</Text>
-          <Text>{checkOut !== -1 ? moment(checkOut * 1000).format('YYYY MMMM DD') : '-'}</Text>
-        </View>
-        <SafeAreaView style={{flex: 1}}>
-          <FlatList data={this.year}
-                    initialNumToRender={3}
-                    windowSize={2}
-                    renderItem={({item}) => (<Month
-                      onSelect={this.selectDay}
-                      checkOut={checkOut}
-                      checkIn={checkIn}
-                      {...item}/>)}
-                    keyExtractor={(month) => month.time.toString()}
-          />
-        </SafeAreaView>
-        {IF.IF(!!checkIn && checkOut !== -1, () =>
-          <TouchableHighlight style={{height: 50, backgroundColor: 'yellow'}}
-                              onPress={this.doneCheck}>
-            <Text style={style.dayText}>
-              done
-            </Text>
-          </TouchableHighlight>).RES
+    createOneMonth(date: Moment): MonthType {
+        const month: MonthType = {name: date.format('MMMM YYYY'), time: date.unix(), days: []};
+        const weekDay = date.day();
+        const days = date.daysInMonth() + weekDay;
+        for (let i = 1; i <= days; i++) {
+            month.days.push({
+                text: weekDay >= i ? '' : i - weekDay,
+                time: weekDay >= i ? 0 : date.unix(),
+            });
+            if (i > weekDay)
+                date.add(1, 'day');
         }
-      </View>
-    );
-  }
+        const ss = (7 - date.day()) % 7;
+        for (let i = 1; i <= ss; i++) {
+            month.days.push({
+                text: '',
+                time: 0,
+            });
+        }
+        return month;
+    }
+
+    createOneYear(): MonthType[] {
+        const date = moment();
+        date.startOf('month');
+        this.convertToZero(date);
+        const oneYear: MonthType[] = [];
+        for (let month = 0; month < 12; month++)
+            oneYear.push(this.createOneMonth(date));
+        return oneYear;
+    }
+
+    selectDay(unix: number) {
+        const {checkIn, checkOut} = this.state;
+        if ((checkIn + 1 && checkOut + 1) || checkIn > unix)
+            this.setState({checkIn: unix, checkOut: -1});
+        else if (checkIn + 1 && unix > checkIn) {
+            this.setState({checkOut: unix});
+            this.nights = Math.floor((unix - this.state.checkIn) / 86400);
+        }
+    }
+
+    doneCheck() {
+        const checkIn = moment.unix(this.state.checkIn);
+        const checkOut = moment.unix(this.state.checkOut);
+        this.props.onSelect({
+            checkIn: {formatted: checkIn.format('YYYY MMMM DD'), value: checkIn.format(this.props.format)},
+            checkOut: {formatted: checkOut.format('YYYY MMMM DD'), value: checkOut.format(this.props.format)},
+        });
+    }
+
+    render() {
+        const {checkOut, checkIn} = this.state;
+        return (
+            <View style={style.container}>
+                <View style={Style.bg__primary}>
+                    <View style={[Style.pb__3, Style.flex__row]}>
+                        <View style={[Style.p__2, Style.w__50]}>
+                            <AppText style={
+                                [
+                                    {fontSize: 18,},
+                                    Style.text__center,
+                                    Style.text__bold,
+                                    Style.text__white
+                                ]}>Check-in</AppText>
+                            <AppText style={
+                                [
+                                    {fontSize: 14,},
+                                    Style.text__center,
+                                    Style.text__white
+                                ]}>{moment(checkIn * 1000).format('YYYY MMMM DD')}</AppText>
+                        </View>
+                        <View style={[Style.p__2, Style.w__50]}>
+                            <AppText style={
+                                [
+                                    {fontSize: 18,},
+                                    Style.text__center,
+                                    Style.text__bold,
+                                    Style.text__white
+                                ]}>Check-out</AppText>
+                            <AppText style={
+                                [
+                                    {fontSize: 14,},
+                                    Style.text__center,
+                                    Style.text__white
+                                ]}>{checkOut !== -1 ? moment(checkOut * 1000).format('YYYY MMMM DD') : '-'}</AppText>
+                        </View>
+                    </View>
+                    <View style={style.dayLabelContainer}>
+                        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(item => <Text
+                            key={item} style={style.dayLabel}>{item}</Text>)}</View>
+                </View>
+                <SafeAreaView style={{flex: 1}}>
+                    <FlatList
+                        data={this.year}
+                        initialNumToRender={3}
+                        windowSize={2}
+                        renderItem={({item}) => (<Month
+                            onSelect={this.selectDay}
+                            checkOut={checkOut}
+                            checkIn={checkIn}
+                            {...item}/>)}
+                        keyExtractor={(month) => month.time.toString()}
+                    />
+                </SafeAreaView>
+                {IF.IF(!!checkIn && checkOut !== -1, () =>
+                    <View style={style.doneContainer}>
+                        <Button style={style.doneBtn}
+                                onPress={this.doneCheck}>
+                            <AppText style={style.doneText}>
+                                DONE
+                                <AppText style={style.dontNightText}>
+                                    {` (${this.nights} Night${this.nights > 1 ? 's' : ''})`}
+                                </AppText>
+                            </AppText>
+                        </Button>
+                    </View>).RES
+                }
+            </View>
+        );
+    }
 }
 
 
--- src/Components/DatePicker/DatepickerStyles.ts
+++ src/Components/DatePicker/DatepickerStyles.ts
@@ -1,55 +1,83 @@
 import {StyleSheet} from 'react-native';
+import {COLOR_PRIMARY, COLOR_WARNING, COLOR_WHITE, SHADOW_LG_XX} from "../../../native-base-theme/variables/config";
 
 
 const DatepickerStyles = StyleSheet.create({
     container: {
         width: '100%',
         flex: 1,
-        paddingTop: 15,
     },
     monthBox: {
         width: '100%',
         display: 'flex',
         flexWrap: 'wrap',
         flexDirection: 'row',
-        // paddingLeft: 10,
-
+        paddingBottom: 30,
+        justifyContent: 'center',
     },
     day: {
         width: '14%',
-        // aspectRatio: 1,
-        height: 30,
-        marginVertical: 2.5,
-        // backgroundColor: `rgba(${0xd2},${0xd2},${0xd2},0.2)`,
+        height: 40,
+        marginVertical: 4,
     },
     firstDay: {
-        borderBottomLeftRadius: 3,
-        borderTopLeftRadius: 3,
-        backgroundColor: 'red',
-
+        backgroundColor: '#0064b9',
     },
     midDay: {
-        backgroundColor: `rgba(${0xff},0,0,.5)`,
+        backgroundColor: '#0089ff',
     },
     endDay: {
-        borderBottomRightRadius: 3,
-        borderTopRightRadius: 3,
-        backgroundColor: 'red',
+        backgroundColor: '#0064b9',
     },
     disabledDay: {
-        backgroundColor: 'blue',
+        paddingHorizontal: 5,
+    },
+    monthName: {
+        width: '100%',
+        paddingVertical: 7,
+        textAlign: 'center',
+        fontSize: 16,
     },
     dayText: {
         width: '100%',
         height: '100%',
         textAlignVertical: 'center',
         textAlign: 'center',
+        fontSize: 14,
     },
-    monthName: {
-        width: '100%',
-        paddingVertical: 7,
+    dayLabelContainer:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingBottom: 7,
+    },
+    dayLabel:{
+        color: COLOR_WHITE,
+        width: '14%',
         textAlign: 'center',
+        fontWeight: 'bold'
     },
+    doneContainer: {
+        padding: 10,
+        ...SHADOW_LG_XX,
+        backgroundColor: COLOR_WHITE,
+    },
+    doneBtn: {
+        width: '100%',
+        height: 50,
+        backgroundColor: COLOR_PRIMARY,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    doneText: {
+        fontWeight: "bold",
+        color: COLOR_WHITE,
+        marginHorizontal: 15,
+    },
+    dontNightText: {
+        color: COLOR_WHITE,
+        fontSize: 14,
+    },
+
 });
 
 export default DatepickerStyles;
