d3952fa29fd0826a0a4da4cc1848932debd9cd8d
soltani <amirmohhamadsotlani@gmail.com>
2020-09-22 15:10:45 +0330

    fix

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {RootStateInterface} from '../Typescript';
-import RNRestart from 'react-native-restart';
 import {default as SearchRoute} from './search.route';
 import {default as HotelsRoute} from './hotels.route';
 import {Route} from 'react-router-native';
@@ -20,13 +19,6 @@ class Routes extends Component<Props> {
   constructor(props: Props) {
     super(props);
     Translator(props.language, props.rtl, props.json!);
-  }
-
-  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<{}>, snapshot?: any) {
-    if (this.props.language !== prevProps.language) {
-      Translator(this.props.language, this.props.rtl, this.props.json!);
-      RNRestart.Restart();
-    }
   }
 
 
--- src/Store/Sagas/change-language.saga.ts
+++ src/Store/Sagas/change-language.saga.ts
@@ -4,6 +4,8 @@ import axios, {AxiosResponse} from 'axios';
 import {LANGUAGE_URL} from '../../URLS';
 import Storage from '../../Lib/Storage';
 import {RootStateInterface} from '../../Typescript';
+import Translator from '../../Lib/Languages';
+import RNRestart from 'react-native-restart';
 
 function* ChangeLanguage({payload: {rtl, lang}}: ChangeLanguageType) {
   try {
@@ -13,7 +15,9 @@ function* ChangeLanguage({payload: {rtl, lang}}: ChangeLanguageType) {
       currency: state.appReducer.currency,
     }));
     yield Storage.save({key: 'app-data', expires: null, data: {track_code, currency, rtl, lang}});
+    Translator(lang, rtl, response.data.result);
     yield put(SetLanguage({lang, rtl, json: response.data.result}));
+    RNRestart.Restart();
   } catch (e) {
 
   }
