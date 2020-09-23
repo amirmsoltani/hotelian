178ef9719c90d9f796703bbcfa7af3b0ddb77f77
soltani <amirmohhamadsotlani@gmail.com>
2020-09-23 15:49:25 +0330

    fix language

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,6 +1,6 @@
 // export const API_URL = 'http://192.168.100.24:9000';
 export const API_URL = 'http://app.hotelian.com';
-export const LANGUAGE_URL = 'http://hotelian.com/api/get-translates?lang=';
+export const LANGUAGE_URL = 'https://hotelian.com/api/get-translates?lang=';
 export const DESTINATIONS_URL = API_URL + '/hotel/destination-auto-complete';
 export const NATIONALITIES_URL = API_URL + '/hotel/nationality-auto-complete';
 export const HOTEL_SEARCH_URL = API_URL + '/hotel/search';
