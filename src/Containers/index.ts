c00d257ea033958a08290533d9956019c4d0f04c
moodi <moodizone@gmail.com>
2020-09-23 10:02:13 +0330

    app modal added

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,4 +1,5 @@
 export * from './search.tabbar';
+export {default as SearchNavBar} from './search.navbar/search.navbar';
 export {default as AppText} from "./app-text/app-text";
 export {default as AppRow} from "./app-row/app-row";
-export {default as SearchNavBar} from './search.navbar/search.navbar';
+export {default as AppModal} from './app-modal/app-modal';
