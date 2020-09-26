ac4d8fca5418093298601c94615c512c64c8824b
moodi <moodizone@gmail.com>
2020-09-26 14:35:41 +0330

    migrate to REZA

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://192.168.100.24:9000';
-export const API_URL = 'http://app.hotelian.com';
+export const API_URL = 'http://hotelian.api';
+// export const API_URL = 'http://app.hotelian.com';
 export const LANGUAGE_URL = 'https://hotelian.com/api/get-translates?lang=';
 export const DESTINATIONS_URL = API_URL + '/hotel/destination-auto-complete';
 export const NATIONALITIES_URL = API_URL + '/hotel/nationality-auto-complete';
