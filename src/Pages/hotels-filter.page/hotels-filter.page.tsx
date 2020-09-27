fa4784b80a6085eb2d6af2e90496673bf9a646a9
moodi <moodizone@gmail.com>
2020-09-27 12:20:52 +0330

    nvm

--- src/Pages/hotels-filter.page/hotels-filter.page.tsx
+++ src/Pages/hotels-filter.page/hotels-filter.page.tsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {connect, ConnectedProps} from 'react-redux';
-import {Card, Content, Footer} from 'native-base';
+import {Body, Button, Footer, Header, Icon, Left, Right, Subtitle, Title} from 'native-base';
 import {RootStateInterface} from '../../Typescript';
 import {ApplyHotelsFilters} from '../../Store/Actions';
 import {Actions} from 'react-native-router-flux';
 import {Conditional, HotelsFilters, If} from '../../Components';
+import {Style} from "../../Styles";
+import style from "../search.page/search-page.styles";
+import {SHADOW_LG_XX} from "../../../native-base-theme/variables/config";
+import {AppText} from "../../Containers";
 
 const mapStateToProps = ({hotelsReducer: {filter, change_filter}}: RootStateInterface) => ({
   structure: filter!.structure,
@@ -21,7 +25,7 @@ const mapDispatchToProps = {
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type Props = ConnectedProps<typeof connector>;
 
-class HotelsFilterPage extends Component<Props, {[key: string]: {indexes: number[], name: string}}> {
+class HotelsFilterPage extends Component<Props, { [key: string]: { indexes: number[], name: string } }> {
   static readonly filters = ['stars', 'boardTypes', 'locations', 'rangePrice'];
   change_filter: number;
 
@@ -44,36 +48,50 @@ class HotelsFilterPage extends Component<Props, {[key: string]: {indexes: number
     const {numbers, structure} = this.props;
     return (
       <>
+        <Header style={[Style.bg__primary]}>
+          <Left>
+            <Button transparent>
+              <Icon type={'MaterialIcons'} name='keyboard-backspace'
+                    style={[Style.f__30, Style.text__white,]}/>
+            </Button>
+          </Left>
+          <Body><Title>Set your filters</Title></Body>
+          <Right>
+            <Button transparent>
+              <Subtitle>RESET</Subtitle>
+            </Button>
+          </Right>
+        </Header>
+        <ScrollView style={[Style.bg__white]}>
+          <View style={[style.wrapper, Style.mb__0]}>
 
-        {/*<StatusBar animated={true} hidden={true}/>*/}
-        <Content>
-          <Card>
             {Object.keys(structure).filter(name => HotelsFilterPage.filters.includes(name)).map(name =>
               // @ts-ignore
               <HotelsFilters structure={structure[name]} actives={this.state} length={numbers} name={name} key={name}
                              ChangeFilters={this.setState}/>)
             }
-          </Card>
-        </Content>
-        <Conditional>
-          <If
-            condition={this.props.change_filter !== this.change_filter}>
-            <Footer style={{backgroundColor: 'white'}}>
-              <TouchableOpacity style={{
-                width: '100%',
-                backgroundColor: 'rgba(30,40,50,.6)',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-                                onPress={() => Actions.jump('hotels')}
-              >
-                <Text>
-                  Apply
-                </Text>
-              </TouchableOpacity>
-            </Footer>
-          </If>
-        </Conditional>
+          </View>
+        </ScrollView>
+        <Footer style={[SHADOW_LG_XX]}>
+          <Conditional>
+            <If
+              condition={this.props.change_filter !== this.change_filter}>
+              <Footer style={{backgroundColor: 'white'}}>
+                <TouchableOpacity
+                  style={{
+                    width: '100%',
+                    backgroundColor: 'rgba(30,40,50,.6)',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                  onPress={() => Actions.jump('hotels')}>
+                  <Text>Apply</Text>
+                </TouchableOpacity>
+              </Footer>
+            </If>
+          </Conditional>
+          <AppText>Footer goes here !!!</AppText>
+        </Footer>
       </>
     );
   }
