195d2e5b43a16cdd32635b62bd93105f92d3ed32
soltani <amirmohhamadsotlani@gmail.com>
2020-09-30 08:26:18 +0330

    add interfaces

---
+++ src/Typescript/Interfaces/rooms-details.interface.ts
@@ -0,0 +1,20 @@
+import {DateType, HotelType, NationalityType, RoomType} from '../Types';
+import {HotelOptionInterface} from './hotel-option.interface';
+
+export interface RoomsResponseInterface {
+  currency: string;
+  expire: number;
+  checkin: DateType;
+  checkout: DateType;
+  night_count: number;
+  rooms_count: number;
+  adults: number;
+  children: number;
+  city: string;
+  country: string;
+  nationality: NationalityType;
+  req_rooms: RoomType[];
+  hotel: HotelType;
+  options: HotelOptionInterface[];
+
+}
