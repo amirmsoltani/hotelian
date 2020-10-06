80eee1c8f29dda1b478126f393c97c9628de2abb
moodi <moodizone@gmail.com>
2020-10-06 14:29:41 +0330

    dynamic text from store

--- src/Forms/modify-search/modify-search.from.tsx
+++ src/Forms/modify-search/modify-search.from.tsx
@@ -1,51 +1,72 @@
 import React from 'react';
 import {View} from "react-native";
-import {Button} from "native-base";
+import {Button, Form} from "native-base";
 import {useNavigation} from '@react-navigation/native';
-
 
 import {Style} from "Styles";
 import ModifyRow from "./modify-row/modify-row";
-import {AppText} from "../../Containers";
-import {translate} from "../../Lib/Languages";
+import {AppText} from "Containers";
+import {translate} from "Lib/Languages";
+import {RootStateInterface} from "../../Typescript/Interfaces";
+import {connect, ConnectedProps} from "react-redux";
+import {AcceptSearchForm} from "../../Store/Actions/search.actions";
 
-const ModifySearchFrom = () => {
+const mapStateToProps = ({searchReducer: {form_data}}: RootStateInterface) => ({
+  rooms: form_data.rooms,
+  nationality: form_data.nationality,
+  checkIn: form_data.checkIn?.formatted,
+  checkOut: form_data.checkOut?.formatted,
+  destination: form_data.destination,
+  adultCounts: form_data.adultCounts,
+  childCounts: form_data.childCounts,
+});
+const connector = connect(mapStateToProps, {AcceptSearchForm});
+type Props = ConnectedProps<typeof connector>
+
+
+const ModifySearchFrom = ({rooms, nationality, checkOut, checkIn, destination, adultCounts, childCounts, ...props}: Props) => {
 
   const navigation = useNavigation();
   return (
-    <View style={[Style.w__100, Style.bg__white, Style.py__3]}>
+    <Form style={[Style.w__100, Style.bg__white, Style.py__3]}>
 
       {/*destination*/}
       <ModifyRow
         onPress={() => navigation.navigate('destination')}
         iconName={'location'} iconType={'Octicons'}
-        text={'Madrid, Spain Madrid, Spain Madrid, Spain Madrid, Spain Madrid, Spain'}/>
+        text={destination ?
+          destination.dest_type === 'city' ? destination.text : `${destination.label}, ${destination.text}`
+          : translate('where-are-you-going?')
+        }/>
 
       {/*checkin-out*/}
       <ModifyRow
         onPress={() => navigation.navigate('datepicker')}
         iconName={'calendar'} iconType={'Octicons'}
-        text={`${'05 September 2020'}  |  ${'05 September 2020'}`}/>
+        text={checkIn ? `${checkIn}  |  ${checkOut}` : `${translate('check-in')}  |  ${translate('check-out')}`}/>
 
       {/*nationality*/}
       <ModifyRow
         onPress={() => navigation.navigate('nationality')}
         iconName={'flag-outline'} iconType={'Ionicons'}
-        text={`CN, China`}/>
+        text={nationality ? nationality.name : translate('enter-your-nationality')}/>
 
       {/*guests*/}
       <ModifyRow
         onPress={() => navigation.navigate('rooms')}
         iconName={'person-add-outline'} iconType={'Ionicons'}
-        text={`${1} Room  |  ${2} Adults  |  ${0} Children`}/>
+        text={
+          `${rooms?.length} ${translate('room')}${!!rooms && rooms?.length > 1 ? 's' : ''}  |  ` +
+          `${adultCounts} ${translate('adult')}${!!adultCounts && adultCounts > 1 ? 's' : ''}  |  ` +
+          `${childCounts} ${translate('children')}`}/>
 
       <View style={[Style.p__3,]}>
-        <Button block>
+        <Button block onPress={props.AcceptSearchForm}>
           <AppText style={[Style.text__white, Style.text__upperCase]}>{translate('search')}</AppText>
         </Button>
       </View>
-    </View>
+    </Form>
   );
 };
 
-export default ModifySearchFrom;
+export default connector(ModifySearchFrom);
