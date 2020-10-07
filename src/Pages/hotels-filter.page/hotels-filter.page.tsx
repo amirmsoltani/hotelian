d0cdec0515f0f57655ae2b47af3c5cc06d3a4f9c
moodi <moodizone@gmail.com>
2020-10-07 12:08:48 +0330

    flatlist active filters

--- src/Pages/hotels-filter.page/hotels-filter.page.tsx
+++ src/Pages/hotels-filter.page/hotels-filter.page.tsx
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {FlatList, ScrollView, View} from 'react-native';
-import {Body, Button, Header, Icon, Left, Right} from 'native-base';
+import {Body, Button, Header, Left, Right} from 'native-base';
 import {StackScreenProps} from '@react-navigation/stack';
 
 import {RootStateInterface} from 'Typescript';
@@ -11,7 +11,7 @@ import {Style} from 'Styles';
 import style from '../search.page/search-page.styles';
 import {AppText, AppTitle, BackNavigation} from 'Containers';
 import {translate, translate as t} from "../../Lib/Languages";
-import {COLOR_PRIMARY, SHADOW_LG_XX} from "../../../native-base-theme/variables/config";
+import {BORDER_RADIUS_SM, COLOR_PRIMARY, SHADOW_LG_XX} from "../../../native-base-theme/variables/config";
 
 const mapStateToProps = ({hotelsReducer: {filter, change_filter}}: RootStateInterface) => ({
   structure: filter!.structure,
@@ -51,7 +51,6 @@ class HotelsFilterPage extends PureComponent<Props, { filters: { [key: string]: 
   render() {
     const activesArray = !this.props.actives || !Object.keys(this.props.actives).length ? [] :
       Object.keys(this.props.actives).map(item => ({key: item, value: this.props.actives![item]}));
-    console.log(activesArray);
     const {numbers, structure} = this.props;
     const canReset = Object.keys(this.state.filters).length > 0;
     return (
@@ -85,42 +84,24 @@ class HotelsFilterPage extends PureComponent<Props, { filters: { [key: string]: 
           <If condition={true}>
             <View style={[Style.p__1, Style.bg__white, Style.flex__column, SHADOW_LG_XX]}>
               <FlatList
-                style={[Style.mb__1,]}
                 data={activesArray}
                 horizontal={true}
                 keyExtractor={item => item.key}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({item}) => {
                   return <Button
-                    transparent style={[Style.px__2]}
+                    transparent style={[Style.pr__1]}
                     activeOpacity={1}>
-                    <View style={[
-                      {borderWidth: .5, borderColor: COLOR_PRIMARY, borderRadius: 25},
-                      Style.bg__white,
-                      Style.px__2,
-                      Style.py__1,
-                    ]}>
-                      <View style={[Style.flex__row, Style.align__items_center, Style.p__1, Style.align__items_center]}>
-                        <Icon style={[Style.f__12, Style.px__0, Style.ml__0, Style.mr__1]} name='close'
-                              type='Ionicons'/>
-                        {/*<Conditional>*/}
-                        {/*  <If condition={item.value.name === 'stars'}>*/}
-                        {/*    {new Array(+item.key).map(_ =>*/}
-                        {/*      <Icon type='AntDesign' name='staro'*/}
-                        {/*            style={[Style.f__16, Style.text__primary]}/>)}*/}
-                        {/*  </If>*/}
-                        {/*  <Else>*/}
+                    <View style={[{borderWidth: .5, borderColor: COLOR_PRIMARY, borderRadius: BORDER_RADIUS_SM,},
+                      Style.mr__1, Style.p__1, Style.bg__white]}>
+                      <View style={[Style.flex__row, Style.align__items_center,]}>
                         <AppText style={[Style.text__primary, Style.mr__1, Style.f__10]}>
-                          {item.key} {item.value.name}
+                          {item.key} {item.value.name === 'stars' ? (+item.key > 1 ? 'stars' : 'star') : ''}
                         </AppText>
-                        {/*  </Else>*/}
-                        {/*</Conditional>*/}
                       </View>
                     </View>
                   </Button>
-                }
-                }
-              />
+                }}/>
               <Button block style={[Style.bg__primary, Style.w__100]}
                       onPress={() => this.props.navigation.navigate('hotels')}>
                 <AppText style={[Style.text__white, Style.text__bold]}>{translate('apply')}</AppText>
