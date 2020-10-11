3754938f9761e646697ae439c6ac7c65518199bb
moodi <moodizone@gmail.com>
2020-10-11 17:19:20 +0330

    hotel facilities screen loading

---
+++ src/Components/hotel-images/hotel-images.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotelImages = () => {
+  return (
+    <div>
+      
+      </div>
+  );
+};
+
+export default HotelImages;
---
+++ src/Components/progressive-image/progressive-image.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProgressiveImage = () => {
+  return (
+    <div>
+      
+      </div>
+  );
+};
+
+export default ProgressiveImage;
