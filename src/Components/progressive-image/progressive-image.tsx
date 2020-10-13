50078ff3e141665e9c4261766da345eb45515d20
moodi <moodizone@gmail.com>
2020-10-13 17:15:09 +0330

    added

--- src/Components/progressive-image/progressive-image.tsx
+++ src/Components/progressive-image/progressive-image.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {Image, ImageProps, View} from "react-native";
 import {MUTED_LIGHT_XXX} from "../../../native-base-theme/variables/config";
 
-const ProgressiveImage = (props: ImageProps) => {
+const ProgressiveImage = (props: ImageProps & { bgColor?: string }) => {
   return (
-    <View style={{backgroundColor: MUTED_LIGHT_XXX}}>
+    <View style={{backgroundColor: props.bgColor || MUTED_LIGHT_XXX}}>
       <Image {...props} />
     </View>
   );
