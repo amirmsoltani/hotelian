3328944601c1a85bae5ee2f93b69ab16bd49ae4b
amirmsoltani <amirmohhamadsoltani@gmail.com>
2020-11-15 09:34:09 +0330

    add object-tool pakage

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,9 +1,10 @@
-// export const API_URL = 'http://hotelian.api';
-export const API_URL = 'http://app.hotelian.com';
+export const API_URL = 'http://hotelian.api';
+// export const API_URL = 'http://app.hotelian.com';
 // export const API_URL = 'http://192.168.100.24:9091';
 // export const LANGUAGE_URL = 'http://b2c.hotelian/api/get-translates?lang=';
-export const LANGUAGE_URL = 'http://hotelian.com/api/get-translates?lang=';
-// export const LANGUAGE_URL = 'http://192.168.100.24:9090/api/get-translates?lang=';
+// export const LANGUAGE_URL = 'https://hotelian.com/api/get-translates?lang=';
+export const LANGUAGE_URL =
+  'http://192.168.100.24:9090/api/get-translates?lang=';
 export const INITIAL_URL = API_URL + '/general/initialize';
 export const DESTINATIONS_URL = API_URL + '/hotel/destination-auto-complete';
 export const NATIONALITIES_URL = API_URL + '/hotel/nationality-auto-complete';
@@ -12,6 +13,6 @@ export const HOTEL_SEARCH_RESULT_URL = API_URL + '/hotel/get-search-result';
 export const USER_TRACK_CODE_URL = API_URL + '/general/get-user-track-code';
 export const HOTEL_DETAILS = API_URL + '/hotel/get-hotel-details';
 export const HOTEL_ROOMS = API_URL + '/hotel/get-hotel-rooms';
-
+// TODO hotel_id hotel_id
 export const USER_INITIAL_ERROR_MESSAGE = 'something wrong please try again';
 export const INTERNET_CONNECTION_ERROR = 'internet connection';
