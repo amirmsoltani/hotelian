11bf18b2d947594520075e12c2a3182d80595dc1
moodi <moodizone@gmail.com>
2020-11-21 17:16:27 +0330

    guest form almost added

--- src/Forms/guest-form/adult-form/adult-form.tsx
+++ src/Forms/guest-form/adult-form/adult-form.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import {View} from "react-native";
+import {Icon, Item, Picker} from "native-base";
 
-const AdultForm = () => {
+import {AppText, FormInput} from "../../../Containers";
+import {translate} from "../../../Lib/Languages";
+import {Style} from "../../../Styles";
+import {BORDER_RADIUS_SM} from "../../../../native-base-theme/variables/config";
+
+
+type propsType = {
+  room_number: number;
+}
+
+const AdultForm: FunctionComponent<{ data: propsType }> = (props) => {
+
   return (
-    <View>
+    <View style={[Style.p__3, Style.bg__white, {borderRadius: BORDER_RADIUS_SM,}]}>
+
+      {/*form header*/}
+      <View style={[Style.mb__3]}>
+        <View style={[Style.flex__row, Style.align__items_center]}>
+          <AppText style={[Style.f__14]}>{translate('adult')} </AppText>
+          <AppText style={[Style.f__14]}>(</AppText>
+          <Icon style={[Style.f__14]} name='man-outline' type={"Ionicons"}/>
+          <AppText style={[Style.f__14]}> | </AppText>
+          <Icon style={[Style.f__14]} name='woman-outline' type={"Ionicons"}/>
+          <AppText style={[Style.f__14]}>)</AppText>
+        </View>
+      </View>
+
+      {/*inputs*/}
+      <View>
+        <View style={[Style.mb__3]}>
+          <FormInput data={{label: translate('first-name')}}/>
+        </View>
+        <View>
+          <FormInput data={{label: translate('last-name')}}/>
+        </View>
+      </View>
+
+      {/*picker*/}
+      <View>
+        <Item picker>
+          <Picker
+            mode="dropdown"
+            iosIcon={<Icon name="arrow-down"/>}
+            style={{width: undefined}}
+            placeholder={translate('title')}
+            placeholderStyle={{color: "blue"}}
+            placeholderIconColor="red"
+            // selectedValue={}
+            // onValueChange={}
+          >
+            <Picker.Item label={translate('male')} value="0"/>
+            <Picker.Item label={translate('female')} value="1"/>
+            <Picker.Item label={translate('other')} value="2"/>
+          </Picker>
+        </Item>
+      </View>
 
     </View>
   );
+
+
 };
 
 export default AdultForm;
--- src/Forms/guest-form/child-form/child-form.tsx
+++ src/Forms/guest-form/child-form/child-form.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import {View} from "react-native";
-import {AppText} from "../../../Containers";
 
-const ChildForm = () => {
+import {AppText, FormInput} from "../../../Containers";
+import {translate} from "../../../Lib/Languages";
+import {Style} from "../../../Styles";
+import {BORDER_RADIUS_SM} from "../../../../native-base-theme/variables/config";
+
+
+type propsType = {
+  room_number: number;
+  child_age: number;
+}
+
+const ChildForm: FunctionComponent<{ data: propsType }> = (props) => {
+
   return (
-    <View>
-      <AppText></AppText>
+    <View style={[Style.p__3, Style.bg__white, {borderRadius: BORDER_RADIUS_SM,}]}>
+
+      {/*form header*/}
+      <View style={[Style.mb__3]}>
+        <View style={[Style.flex__row, Style.align__items_center]}>
+          <AppText style={[Style.f__14]}>{translate('child')} </AppText>
+          <AppText style={[Style.f__14, Style.text__light]}>
+            ({props.data.child_age} {props.data.child_age > 1 ? translate('years-old') : translate('year-old')})</AppText>
+        </View>
+      </View>
+
+      {/*inputs*/}
+      <View>
+        <View style={[Style.mb__3]}>
+          <FormInput data={{label: translate('first-name')}}/>
+        </View>
+        <View>
+          <FormInput data={{label: translate('last-name')}}/>
+        </View>
+      </View>
+
     </View>
   );
+
+
 };
 
 export default ChildForm;
