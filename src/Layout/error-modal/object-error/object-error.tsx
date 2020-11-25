c554fe8224ba59fff5ea99fb3afba6ec5bea087a
moodi <moodizone@gmail.com>
2020-11-25 10:40:39 +0330

    remove default caption

--- src/Layout/error-modal/object-error/object-error.tsx
+++ src/Layout/error-modal/object-error/object-error.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import {View} from "react-native";
 import {AppText} from "../../../Containers";
 import {Style} from "../../../Styles";
-import {translate} from "../../../Lib/Languages";
 
 const ObjectError = (props: { errors: { [key: string]: string } }) => {
   return (
     <View>
-      <AppText style={[Style.f__14, Style.mb__3]} firstLetter>
-        {Object.values(props.errors).length > 1 ? translate('following-errors-are-reported') : translate('following-error-is-reported')}:
-      </AppText>
       {Object.values(props.errors).map((error, index) => (
-        <View style={[Style.flex__row]}>
+        <View key={index + ''} style={[Style.flex__row]}>
           <AppText style={[Style.f__14, Style.mr__1]} firstLetter>
             {Object.keys(props.errors)[index]} :</AppText>
           <AppText style={[Style.text__light, Style.f__14]} firstLetter>
