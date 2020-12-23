d8632b87e37ccd9445a20517f696c4805bc3886c
moodi <moodizone@gmail.com>
2020-12-23 09:53:55 +0330

    nvm

--- src/Components/table-list/actions/actions.tsx
+++ src/Components/table-list/actions/actions.tsx
@@ -1,8 +1,13 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState} from "react";
 import {Button, Icon, Left, List, ListItem, Right, View} from "native-base";
 import {StyleSheet, TouchableNativeFeedback} from "react-native";
 
-import {BORDER_RADIUS_SM, MUTED_LIGHT_XX, SHADOW_SM_X} from "../../../../native-base-theme/variables/config";
+import {
+  BORDER_RADIUS_SM,
+  COLOR_PRIMARY,
+  MUTED_LIGHT_XX,
+  SHADOW_SM_X,
+} from "../../../../native-base-theme/variables/config";
 import {Style} from "../../../Styles";
 import {AppModal, AppText} from "../../../Containers";
 import {translate} from "../../../Lib/Languages";
@@ -10,77 +15,143 @@ import {Conditional, If} from "../../index";
 import TableList, {context_type} from "../table-list";
 import Input from "./input/input";
 
-type modal_type = 'filter' | 'search' | null;
+type modal_type = "filter" | "search" | null;
 type state_type<T> = {
-
   /** show / hide filters modal */
   modal_visibility: boolean;
 
   /** type of presenting modal */
   modal_type: modal_type;
-}
+};
 
 function Actions<T extends object>() {
-  const [state, setState] = useState<state_type<T>>({modal_visibility: false, modal_type: null});
-  const {status, filters, activated_filter, onSelectFilter, onRemoveFilter, searched_value, input_search, onSearchTerm, onClearTerm} =
-    useContext((TableList.contextType)) as context_type<T>;
+  const [state, setState] = useState<state_type<T>>({
+    modal_visibility: false,
+    modal_type: null,
+  });
+  const {
+    status,
+    filters,
+    activated_filter,
+    onSelectFilter,
+    onRemoveFilter,
+    searched_value,
+    input_search,
+    onSearchTerm,
+    onClearTerm,
+  } = useContext(TableList.contextType) as context_type<T>;
 
-  const showModal = (type: modal_type) => setState({...state, modal_visibility: true, modal_type: type});
+  const showModal = (type: modal_type) =>
+    setState({...state, modal_visibility: true, modal_type: type});
   const hideModal = () => setState({...state, modal_visibility: false});
 
   //for prevent re-rendering on input change
   let input_value: string | undefined = searched_value;
-  const getValue = (v: string | undefined) => input_value = v;
-
+  const getValue = (v: string | undefined) => (input_value = v);
 
   return (
-    <View style={[styles.h, SHADOW_SM_X, Style.bg__white, Style.flex__row,
-      Style.justify__content_between, Style.align__items_center]}>
-
+    <View
+      style={[
+        styles.h,
+        SHADOW_SM_X,
+        Style.bg__white,
+        Style.flex__row,
+        Style.justify__content_between,
+        Style.align__items_center,
+      ]}
+    >
       {/*filter*/}
       <TouchableNativeFeedback
-        onPress={() => showModal('filter')}
-        disabled={status !== 'ok' || !filters?.length}>
-        <View style={[Style.col__6, Style.h__100, Style.flex__row,
-          Style.justify__content_center, Style.align__items_center,]}>
-          <Icon type="AntDesign" name="filter" style={[Style.f__16, Style.text__info]}/>
-          <AppText style={[Style.ml__2, Style.text__primary]}>{translate('filter')}</AppText>
+        onPress={() => showModal("filter")}
+        disabled={status !== "ok" || !filters?.length}
+      >
+        <View
+          style={[
+            Style.col__6,
+            Style.h__100,
+            Style.flex__row,
+            Style.justify__content_center,
+            Style.align__items_center,
+          ]}
+        >
+          <Icon
+            type="AntDesign"
+            name="filter"
+            style={[Style.f__16, Style.text__info]}
+          />
+          <AppText style={[Style.ml__2, Style.text__primary]}>
+            {translate("filter")}
+          </AppText>
           <Conditional>
             <If condition={Boolean(activated_filter)}>
               <View style={[Style.bg__danger, styles.bullet]}/>
             </If>
             <If condition={Boolean(filters?.length)}>
               <AppModal
-                backdrop visibility={state.modal_visibility && state.modal_type === 'filter'} position={"bottom"}
-                onClose={() => hideModal()}>
-                <View style={[Style.w__100, {borderRadius: BORDER_RADIUS_SM},]}>
+                backdrop
+                visibility={
+                  state.modal_visibility && state.modal_type === "filter"
+                }
+                position={"bottom"}
+                onClose={() => hideModal()}
+              >
+                <View
+                  style={[Style.w__100, {borderRadius: BORDER_RADIUS_SM}]}
+                >
                   <View style={[Style.bg__white, Style.mt__auto]}>
                     <List>
-                      <ListItem style={[Style.px__3, Style.mx__0]} key='all'
-                                onPress={() => {
-                                  onRemoveFilter();
-                                  hideModal();
-                                }}>
-                        <Left><AppText>{translate('all')}</AppText></Left>
+                      <ListItem
+                        style={[Style.px__3, Style.mx__0]}
+                        key="all"
+                        onPress={() => {
+                          onRemoveFilter();
+                          hideModal();
+                        }}
+                      >
+                        <Left>
+                          <AppText>{translate("all")}</AppText>
+                        </Left>
                         <Right>
-                          <Icon type={'MaterialIcons'}
-                                style={[activated_filter === null ? Style.text__info : null]}
-                                name={`radio-button-${activated_filter === null ? '' : 'un'}checked`}/>
+                          <Icon
+                            type={"MaterialIcons"}
+                            style={[
+                              activated_filter === null
+                                ? Style.text__info
+                                : null,
+                            ]}
+                            name={`radio-button-${
+                              activated_filter === null ? "" : "un"
+                            }checked`}
+                          />
                         </Right>
                       </ListItem>
-                      {filters!.map((item, index) =>
-                        <ListItem style={[Style.px__3, Style.mx__0]} key={`${index}`}
-                                  onPress={() => {
-                                    if (onSelectFilter) onSelectFilter(item);
-                                    hideModal();
-                                  }}>
-                          <Left><AppText>{item.label}</AppText></Left>
+                      {filters!.map((item, index) => (
+                        <ListItem
+                          style={[Style.px__3, Style.mx__0]}
+                          key={`${index}`}
+                          onPress={() => {
+                            if (onSelectFilter) onSelectFilter(item);
+                            hideModal();
+                          }}
+                        >
+                          <Left>
+                            <AppText>{item.label}</AppText>
+                          </Left>
                           <Right>
-                            <Icon type={'MaterialIcons'}
-                                  style={[activated_filter === item ? Style.text__info : null]}
-                                  name={`radio-button-${activated_filter === item ? '' : 'un'}checked`}/>
+                            <Icon
+                              type={"MaterialIcons"}
+                              style={[
+                                activated_filter === item
+                                  ? Style.text__info
+                                  : null,
+                              ]}
+                              name={`radio-button-${
+                                activated_filter === item ? "" : "un"
+                              }checked`}
+                            />
                           </Right>
-                        </ListItem>)}
+                        </ListItem>
+                      ))}
                     </List>
                   </View>
                 </View>
@@ -95,38 +166,84 @@ function Actions<T extends object>() {
 
       {/*search*/}
       <TouchableNativeFeedback
-        onPress={() => showModal('search')}
-        disabled={status !== 'ok' || !input_search}>
-        <View style={[Style.col__6, Style.h__100, Style.flex__row,
-          Style.justify__content_center, Style.align__items_center,]}>
-          <Icon type="Ionicons" name="search" style={[Style.f__16, Style.text__info]}/>
-          <AppText style={[Style.ml__2, Style.text__primary]}>{translate('search')}</AppText>
+        onPress={() => showModal("search")}
+        disabled={status !== "ok" || !input_search}
+      >
+        <View
+          style={[
+            Style.col__6,
+            Style.h__100,
+            Style.flex__row,
+            Style.justify__content_center,
+            Style.align__items_center,
+          ]}
+        >
+          <Icon
+            type="Ionicons"
+            name="search"
+            style={[Style.f__16, Style.text__info]}
+          />
+          <AppText style={[Style.ml__2, Style.text__primary]}>
+            {translate("search")}
+          </AppText>
           <Conditional>
             <If condition={Boolean(searched_value)}>
               <View style={[Style.bg__danger, styles.bullet]}/>
             </If>
             <If condition={Boolean(input_search)}>
               <AppModal
-                backdrop visibility={state.modal_visibility && state.modal_type === 'search'} position={"bottom"}
-                onClose={() => hideModal()}>
-                <View style={[Style.w__100, {borderRadius: BORDER_RADIUS_SM},]}>
-                  <View style={[Style.bg__white,]}>
+                backdrop
+                visibility={
+                  state.modal_visibility && state.modal_type === "search"
+                }
+                position={"bottom"}
+                onClose={() => hideModal()}
+              >
+                <View
+                  style={[Style.w__100, {borderRadius: BORDER_RADIUS_SM}]}
+                >
+                  <View style={[Style.bg__white]}>
                     <View style={[Style.p__3, Style.mb__3]}>
-                      <Input change={getValue}
-                             value={searched_value}
-                             label={input_search?.label_text}/>
+                      <Input
+                        change={getValue}
+                        value={searched_value}
+                        label={input_search?.label_text}
+                      />
                     </View>
                     <View style={[Style.p__3]}>
-                      <Button onPress={() => {
-                        if (input_value) onSearchTerm(input_value);
-                        else onClearTerm();
-                        hideModal();
-                      }}
-                              block style={[Style.bg__primary]}>
+                      <Button
+                        onPress={() => {
+                          if (input_value) onSearchTerm(input_value);
+                          else onClearTerm();
+                          hideModal();
+                        }}
+                        block
+                        style={[Style.bg__primary]}
+                      >
                         <AppText firstLetter style={[Style.text__white]}>
-                          {translate('search')}</AppText>
+                          {translate("search")}
+                        </AppText>
                       </Button>
                     </View>
+                    <Conditional>
+                      <If condition={Boolean(searched_value)}>
+                        <View style={[Style.px__3, Style.pb__3]}>
+                          <Button
+                            bordered
+                            onPress={() => {
+                              onClearTerm();
+                              hideModal();
+                            }}
+                            block
+                            style={[Style.bg__white, {borderColor: COLOR_PRIMARY,}]}
+                          >
+                            <AppText firstLetter style={[Style.text__primary]}>
+                              {translate("reset")}
+                            </AppText>
+                          </Button>
+                        </View>
+                      </If>
+                    </Conditional>
                   </View>
                 </View>
               </AppModal>
@@ -134,14 +251,20 @@ function Actions<T extends object>() {
           </Conditional>
         </View>
       </TouchableNativeFeedback>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  bullet: {width: 6, height: 6, borderRadius: 3, position: 'absolute', top: 15, right: 15,},
-  divider: {width: 1, height: '70%', backgroundColor: MUTED_LIGHT_XX},
+  bullet: {
+    width: 6,
+    height: 6,
+    borderRadius: 3,
+    position: "absolute",
+    top: 15,
+    right: 15,
+  },
+  divider: {width: 1, height: "70%", backgroundColor: MUTED_LIGHT_XX},
   h: {height: 50},
 });
 
--- src/Components/table-list/table-list.tsx
+++ src/Components/table-list/table-list.tsx
@@ -1,12 +1,11 @@
-import React, {Component, createContext} from 'react';
-import {Content} from "native-base";
+import React, { Component, createContext } from "react";
+import { Content } from "native-base";
 
-import {Conditional, ElIf, Else, ErrorResults, If, NoResults} from "../index";
-import {column_type, filter_type, search_type, status_type} from './types';
-import {translate} from "../../Lib/Languages";
+import { Conditional, ElIf, Else, ErrorResults, If, NoResults } from "../index";
+import { column_type, filter_type, search_type, status_type } from "./types";
+import { translate } from "../../Lib/Languages";
 
 type props_type<T> = {
-
   /** [required] : screen title */
   title: string;
 
@@ -21,10 +20,10 @@ type props_type<T> = {
   status: status_type;
 
   /** [optional] bind button when fetching data has error (e.g fetch again or back  */
-  errorButton?: { click: () => void, label: string },
+  errorButton?: { click: () => void; label: string };
 
   /** [optional] bind button when there is no error */
-  noResults?: { click: () => void, label: string },
+  noResults?: { click: () => void; label: string };
 
   /** table data for calculations (iterate through array as one new record) */
   data: T[];
@@ -42,11 +41,9 @@ type props_type<T> = {
 
   /** [optional] Filter data base on entered term (only apply for one column) */
   input_search?: search_type<T>;
-
 };
 
 type state_type<T> = {
-
   /** hold selected picker item */
   activated_filter: filter_type<T> | null;
 
@@ -58,35 +55,35 @@ type state_type<T> = {
 
   /** flag for resetting search input */
   no_results_by_filter: boolean;
-
 };
 
-export type context_type<T> = props_type<T> & state_type<T> & {
+export type context_type<T> = props_type<T> &
+  state_type<T> & {
+    /** fire when 'non-All' picker selected */
+    onSelectFilter?: (f: filter_type<T>) => void;
 
-  /** fire when 'non-All' picker selected */
-  onSelectFilter?: (f: filter_type<T>) => void,
+    /** fire when 'All' picker selected */
+    onRemoveFilter: () => void;
 
-  /** fire when 'All' picker selected */
-  onRemoveFilter: () => void,
+    /** fire when pressing SEARCH button in search-modal */
+    onSearchTerm: (s: string) => void;
 
-  /** fire when pressing SEARCH button in search-modal */
-  onSearchTerm: (s: string) => void,
+    /** fire when pressing RESET button in search-modal */
+    onClearTerm: () => void;
 
-  /** fire when pressing RESET button in search-modal */
-  onClearTerm: () => void,
+    /** remove all filters */
+    onResetAllFilters: () => void;
+  };
 
-  /** remove all filters */
-  onResetAllFilters: () => void;
-
-};
-
-class TableList<T> extends Component<props_type<T>, state_type<T>, context_type<T>> {
-
+class TableList<T> extends Component<
+  props_type<T>,
+  state_type<T>,
+  context_type<T>
+> {
   static contextType = createContext<context_type<any>>({
-
     //props
-    status: 'ok',
-    title: '',
+    status: "ok",
+    title: "",
     data: [],
     filters: [],
     columns: [],
@@ -98,21 +95,16 @@ class TableList<T> extends Component<props_type<T>, state_type<T>, context_type<
     no_results_by_filter: false,
 
     //functions
-    onSelectFilter: () => {
-    },
-    onRemoveFilter: () => {
-    },
-    onSearchTerm: () => {
-    },
-    onClearTerm: () => {
-    },
-    onResetAllFilters: () => {
-    }
+    onSelectFilter: () => {},
+    onRemoveFilter: () => {},
+    onSearchTerm: () => {},
+    onClearTerm: () => {},
+    onResetAllFilters: () => {},
   });
-  static ListHeader = require('./header/header').default;
-  static ListActions = require('./actions/actions').default;
-  static ListLoading = require('./loading/loading').default;
-  static ListItems = require('./items/items').default;
+  static ListHeader = require("./header/header").default;
+  static ListActions = require("./actions/actions").default;
+  static ListLoading = require("./loading/loading").default;
+  static ListItems = require("./items/items").default;
 
   //=======================================
   // Hooks
@@ -129,152 +121,140 @@ class TableList<T> extends Component<props_type<T>, state_type<T>, context_type<
 
   render() {
     return (
-      <TableList.contextType.Provider value={{
+      <TableList.contextType.Provider
+        value={{
+          //props
+          ...this.props,
 
-        //props
-        ...this.props,
+          //states
+          ...this.state,
 
-        //states
-        ...this.state,
-
-        //handlers
-        onSelectFilter: this.onSelectFilter,
-        onRemoveFilter: this.onRemoveFilter,
-        onSearchTerm: this.onSearchTerm,
-        onClearTerm: this.onClearSearchTerm,
-        onResetAllFilters: this.onResetAllFilters,
-
-      }}>
-
+          //handlers
+          onSelectFilter: this.onSelectFilter,
+          onRemoveFilter: this.onRemoveFilter,
+          onSearchTerm: this.onSearchTerm,
+          onClearTerm: this.onClearSearchTerm,
+          onResetAllFilters: this.onResetAllFilters,
+        }}
+      >
         {/*header*/}
-        <TableList.ListHeader/>
+        <TableList.ListHeader />
 
         {/*actions*/}
-        <TableList.ListActions<T>/>
+        <TableList.ListActions<T> />
 
         {/*loading*/}
-        <TableList.ListLoading<T>/>
+        <TableList.ListLoading<T> />
 
         {/*content*/}
         <Conditional>
-
           {/*ok*/}
-          <If condition={this.props.status === 'ok'}>
+          <If condition={this.props.status === "ok"}>
             <Conditional>
-
               {/*no-results by filter*/}
               <If condition={this.state.no_results_by_filter}>
-                <NoResults data={{
-                  button: {
-                    label: translate('remove-all-filters'),
-                    click: this.onResetAllFilters,
-                  },
-                  text: translate('not-found-text'),
-                  title: translate('not-found-title')
-                }}/>
+                <NoResults
+                  data={{
+                    button: {
+                      label: translate("remove-all-filters"),
+                      click: this.onResetAllFilters,
+                    },
+                    text: translate("not-found-text"),
+                    title: translate("not-found-title"),
+                  }}
+                />
               </If>
               <Else>
                 <TableList.ListItems<T>
                   data={this.props.data}
                   click={this.props.click}
-                  columns={this.props.columns}/>
+                  columns={this.props.columns}
+                />
               </Else>
             </Conditional>
           </If>
 
           {/*error*/}
-          <ElIf condition={this.props.status === 'error'}>
+          <ElIf condition={this.props.status === "error"}>
             <Content>
-              <ErrorResults data={{
-                button: this.props.errorButton,
-                text: translate('error-results-text'),
-                title: translate('error-results-title')
-              }}/>
+              <ErrorResults
+                data={{
+                  button: this.props.errorButton,
+                  text: translate("error-results-text"),
+                  title: translate("error-results-title"),
+                }}
+              />
             </Content>
           </ElIf>
 
           {/*no results*/}
-          <ElIf condition={this.props.status === 'no-result'}>
+          <ElIf condition={this.props.status === "no-result"}>
             <Content>
-
-              <NoResults data={{
-                button: this.props.noResults,
-                text: translate('not-found-text'),
-                title: translate('not-found-title')
-              }}/>
+              <NoResults
+                data={{
+                  button: this.props.noResults,
+                  text: translate("not-found-text"),
+                  title: translate("not-found-title"),
+                }}
+              />
             </Content>
           </ElIf>
-
         </Conditional>
-
       </TableList.contextType.Provider>
     );
   }
-
 
   //=======================================
   // Handlers
   //=======================================
   onSelectFilter = (filter: filter_type<T>) => {
-    const data = this.state.searched_value ?
-      this.selectFilter(filter, this.searchTerm(this.state.searched_value, this.props.data)) :
-      this.selectFilter(filter, this.props.data);
+    const data = this.state.searched_value
+      ? this.selectFilter(
+          filter,
+          this.searchTerm(this.state.searched_value, this.props.data)
+        )
+      : this.selectFilter(filter, this.props.data);
 
     this.setState({
       filtered_data: data,
       activated_filter: filter,
       no_results_by_filter: data.length < 1,
     });
-  }
+  };
 
   onRemoveFilter = () => {
     this.setState({
-      filtered_data: this.state.searched_value ?
-        this.searchTerm(this.state.searched_value, this.props.data) :
-        this.props.data,
+      filtered_data: this.state.searched_value
+        ? this.searchTerm(this.state.searched_value, this.props.data)
+        : this.props.data,
       activated_filter: null,
     });
-  }
+  };
 
   onSearchTerm = (term: string | undefined) => {
-    const data = this.state.activated_filter ?
-      this.searchTerm(term, this.selectFilter(this.state.activated_filter, this.props.data)) :
-      this.searchTerm(term, this.props.data);
+    const data = this.state.activated_filter
+      ? this.searchTerm(
+          term,
+          this.selectFilter(this.state.activated_filter, this.props.data)
+        )
+      : this.searchTerm(term, this.props.data);
 
     this.setState({
       searched_value: term,
       filtered_data: data,
       no_results_by_filter: data.length < 1,
     });
-  }
+  };
 
   onClearSearchTerm = () => {
+    console.log(22);
     this.setState({
-      filtered_data: this.state.activated_filter ?
-        this.searchTerm(this.state.searched_value, this.props.data) :
-        this.props.data,
+      filtered_data: this.state.activated_filter
+        ? this.searchTerm(this.state.searched_value, this.props.data)
+        : this.props.data,
       searched_value: undefined,
     });
-  }
-
-  private searchTerm(term: string | undefined, data: T[]): T[] {
-    if (term?.length && this.props.input_search && data.length &&
-      typeof data[0][this.props.input_search.index] === "string") {
-      return data.filter(item => {
-        const el = item[this.props.input_search?.index!];
-        if (el) return (((el as unknown) as string).indexOf(term.toLowerCase()) > -1);
-        else return false;
-      });
-    }
-    return data;
-  }
-
-  private selectFilter(filter: filter_type<T>, data: T[]): T[] {
-    if (filter) {
-      return data.filter(item => filter.handler(item));
-    }
-    return data;
-  }
+  };
 
   onResetAllFilters = () => {
     this.setState({
@@ -283,8 +263,31 @@ class TableList<T> extends Component<props_type<T>, state_type<T>, context_type<
       searched_value: undefined,
       activated_filter: null,
     });
+  };
+
+  private searchTerm(term: string | undefined, data: T[]): T[] {
+    if (
+      term?.length &&
+      this.props.input_search &&
+      data.length &&
+      typeof data[0][this.props.input_search.index] === "string"
+    ) {
+      return data.filter((item) => {
+        const el = item[this.props.input_search?.index!];
+        if (el)
+          return ((el as unknown) as string).indexOf(term.toLowerCase()) > -1;
+        else return false;
+      });
+    }
+    return data;
   }
 
+  private selectFilter(filter: filter_type<T>, data: T[]): T[] {
+    if (filter) {
+      return data.filter((item) => filter.handler(item));
+    }
+    return data;
+  }
 }
 
 export default TableList;
